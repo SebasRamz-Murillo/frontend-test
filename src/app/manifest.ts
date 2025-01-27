7735680a0984a64b5bac8448e499ab27f871d5ad
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2025-01-27 17:54:48 -0600

    Fix manifest for PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,20 +6,54 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: 'Shop',
     description: 'A shop',
     start_url: '/',
+    scope: '.',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
+    orientation: "portrait",
+    screenshots: [
+        {
+            src: "./img/desktop_view.png",
+            sizes: "1662x955",
+            type: "image/png",
+            form_factor: "narrow",
+            label: "Homescreen of Shop on mobile device"
+        },
+        {
+            src: "./img/desktop_view.png",
+            sizes: "1662x955",
+            type: "image/png",
+            form_factor: "wide",
+            label: "Homescreen of Shop on desktop device"
+        }
+      ],
+    
     icons: [
       {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: './favicon.ico',
+        sizes: '48x48',
+        type: 'image/x-icon',
+        purpose: 'any'
       },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
+        {
+            src: './favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'any'
+        },
+        {
+            src: './favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any'
+        },
+        {
+            src: './icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+        },
+
     ],
   }
 }
